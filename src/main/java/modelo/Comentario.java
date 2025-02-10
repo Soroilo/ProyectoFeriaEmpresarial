130d9b6d110d19f5993061f571f502280e6e558c
Familia Yara <Familia Yara@DESKTOP-5J03NDK>
2025-02-10 15:24:38 -0500

    Se crea el Package servicios y la clase EmpresaService

--- src/main/java/modelo/Comentario.java
+++ src/main/java/modelo/Comentario.java
@@ -10,4 +10,34 @@ public class Comentario {
     private int calificacion;
     private String texto;
     private Stand stand;
+
+
+    public Comentario(Visitante visitante, String fecha, int calificacion, String texto, Stand stand) {
+        this.visitante = visitante;
+        this.fecha = fecha;
+        this.calificacion = calificacion;
+        this.texto = texto;
+        this.stand = stand;
+    }
+
+    // Getters y Setters
+    public Visitante getVisitante() { return visitante; }
+    public void setVisitante(Visitante visitante) { this.visitante = visitante; }
+
+    public String getFecha() { return fecha; }
+    public void setFecha(String fecha) { this.fecha = fecha; }
+
+    public int getCalificacion() { return calificacion; }
+    public void setCalificacion(int calificacion) { this.calificacion = calificacion; }
+
+    public String getTexto() { return texto; }
+    public void setTexto(String texto) { this.texto = texto; }
+
+    public Stand getStand() { return stand; }
+    public void setStand(Stand stand) { this.stand = stand; }
+
+    @Override
+    public String toString() {
+        return "Comentario{" + "visitante=" + visitante + ", fecha=" + fecha + ", calificacion=" + calificacion + ", texto=" + texto + ", stand=" + stand + '}';
+    }
 }
---
+++ src/main/java/servicios/EmpresaService.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servicios;
+
+import modelo.Empresa;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpresaService {
+    private List<Empresa> empresas = new ArrayList<>();
