0a87fc0c1d07a09c4ccec18c5ed610db62438492
Familia Yara <Familia Yara@DESKTOP-5J03NDK>
2025-02-10 14:38:03 -0500

    Creacion clase Empresa

--- src/main/java/modelo/Empresa.java
+++ src/main/java/modelo/Empresa.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Familia Yara
- */
 public class Empresa {
-    
+    private String nombre;
+    private String sector;
+    private String email;
+
+    public Empresa(String nombre, String sector, String email) {
+        this.nombre = nombre;
+        this.sector = sector;
+        this.email = email;
+    }
+
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getSector() { return sector; }
+    public void setSector(String sector) { this.sector = sector; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "nombre=" + nombre + ", sector=" + sector + ", email=" + email + '}';
+    }
 }
